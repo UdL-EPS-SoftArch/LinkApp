209569783c95c23725e78c6e11f11a168a71607a
alexflorensa <florensa14@gmail.com>
2022-04-21 20:49:41 +0200

    Toggle container and margin classes of app body depending on urll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyNFTMkt';
+  title = 'LinkApp';
+  private routesWithoutContainer: string[];
+
+  /**
+   * Add your URL to 'routesWithoutContainer' list if you don't want the body of your page to have a container.
+   * @param router App router
+   */
+  constructor(private router: Router) {
+    this.router = router;
+    this.routesWithoutContainer = [
+      '/meets'
+    ];
+  }
+
+  /**
+   * @returns False if the current URL is contained in 'routesWithoutContainer' list
+   */
+  public includeContainer(): boolean {
+    return !this.routesWithoutContainer.includes(this.router.url);
+  }
+
 }
