2cb5f6d4b3198599bf762c9f7710b6a6bb89b012
Sara Quejido <quejidosara@gmail.com>
2022-04-26 15:45:20 +0200

    Feed component added to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,10 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import {FeedComponent} from './feed/feed/feed.component';
 
 const routes: Routes = [
+  { path: 'feed', component: FeedComponent},
   { path: 'users/create', component: UserRegisterComponent},
   { path: 'users/:id/delete', component: UserDeleteComponent, canActivate: [LoggedInGuard]},
   { path: 'users/:id/edit', component: UserEditComponent, canActivate: [LoggedInGuard]},
--- src/app/feed/feed/feed.component.ts
+++ src/app/feed/feed/feed.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import { UserService } from '../../user/user.service';
+import { User } from '../../login-basic/user';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-feed',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  public user: User;
 
-  constructor() { }
+  constructor(private router: Router,
+              private location: Location,
+              private userService: UserService,
+              private authenticationBasicService: AuthenticationBasicService) { }
 
   ngOnInit(): void {
+    this.user = new User();
   }
 
 }
