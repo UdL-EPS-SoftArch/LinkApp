223cc8eaf66d72350a0b1f8ad1ff89164d265d83
marcresaenrich <marcresaenrich@gmail.com>
2022-04-26 17:09:18 +0200

    Created chat-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {HttpErrorInterceptor} from './error-handler/http-error-interceptor';
 import {AuthenticationBasicService} from './login-basic/authentication-basic.service';
 import {LoggedInGuard} from './login-basic/loggedin.guard';
 import {UserService} from './user/user.service';
+import { ChatListComponent } from './chat/chat-list/chat-list.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {UserService} from './user/user.service';
     UserRegisterComponent,
     UserEditComponent,
     UserDeleteComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    ChatListComponent
   ],
   imports: [
     BrowserModule,
