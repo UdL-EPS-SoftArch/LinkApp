c19c072e98463cc6b05d4d8d552a059b3952e406
gguardiola99 <guillem.escarp@gmail.com>
2022-05-03 16:30:29 +0200

    post created

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PostService} from './post.service';
+import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
+import {User} from '../login-basic/user';
+import {Post} from './post';
+import {Group} from '../group-structure/group';
+import {GroupService} from '../group-structure/group.service';
+
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +14,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  public user: User = new User();
+  public group: Group = new Group();
+  public post: Post = new Post();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private groupService: GroupService,
+              private postService: PostService,
+              private authenticationService: AuthenticationBasicService
+  ) { }
+
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.postService.getResource('1').subscribe(
+      post => {
+        this.post = post;
+      });
   }
 
 }
+
+
+
