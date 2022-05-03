a72c1571ed00b16f85cf5355440fa6ba99b1deb0
marcresaenrich <marcresaenrich@gmail.com>
2022-05-03 16:06:03 +0200

    chat-list.component.ts first version

--- src/app/chat/chat-list/chat-list.component.ts
+++ src/app/chat/chat-list/chat-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChatService } from '../chat.service';
+import { Message } from '../message';
+import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
+
 
 @Component({
   selector: 'app-chat-list',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-list.component.css']
 })
 export class ChatListComponent implements OnInit {
+  public messages: Message[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalMessages = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public router: Router,
+    private messageService: ChatService) {
   }
 
+  ngOnInit(): void {
+    this.messageService.getPage({ pageParams:  { size: this.pageSize }, sort: { creationDate: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Message>) => {
+        this.messages = page.resources;
+        this.totalMessages = page.totalElements;
+      });
+  }
+
+  changePage(): void {
+    this.messageService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { creationDate: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Message>) => this.messages = page.resources);
+  }
+
+  detail(message: Message): void {
+    this.router.navigate(['messages', message.id]);
+  }
 }
