f82441401317305f1bab89d435ebb0ae6e98a976
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-05-05 15:11:43 +0200

    update all dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyNFTMkt'`, () => {
+  it(`should have as title 'LinkUp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyNFTMkt');
+    expect(app.title).toEqual('LinkUp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MyNFTMkt app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('LinkUp app is running!');
   });
 });
