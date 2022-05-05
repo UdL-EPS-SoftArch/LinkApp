f919a9311a0ad92380e3b77e85e13740a9a28d93
Oriol Aguilarm <aguilars99@gmail.com>
2022-05-05 16:45:54 +0200

    Group list in progress

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/internal/Observable';
 import { HateoasResourceOperation, ResourceCollection } from '@lagoshny/ngx-hateoas-client';
 import { Post } from './post';
 import {User} from '../login-basic/user';
+import {Group} from '../group-structure/group';
 
 @Injectable({providedIn: 'root'})
 export class PostService extends HateoasResourceOperation<Post> {
@@ -11,12 +12,22 @@ export class PostService extends HateoasResourceOperation<Post> {
     super(Post);
   }
 
-  public findByGroupContaining(query: string): Observable<ResourceCollection<Post>> {
-    return this.searchCollection('findByGroupContaining', { params: { text: query } });
+  public findByGroup(query: string): Observable<ResourceCollection<Post>> {
+    return this.searchCollection('findByGroup', { params: { text: query } });
   }
 
-  public findByFatherContaining(query: string): Observable<ResourceCollection<Post>> {
-    return this.searchCollection('findByFatherContaining', { params: { text: query } });
+  public findByGroup2(group: Group): Observable<ResourceCollection<Post>> {
+    const options: any = {params: [{key: 'group', value: group.uri}]};
+    return this.searchCollection('findByGroup', options);
+  }
+/*
+  public findByGroupAndFather(group: Group, father: Post): Observable<ResourceCollection<Post>> {
+    const options: any = {params: [{key: 'round', value: uri}]};
+    return this.searchCollection('findByGroupAndFather', { params: { text: query } });
+  }
+*/
+  public findByFather(query: string): Observable<ResourceCollection<Post>> {
+    return this.searchCollection('findByFather', { params: { text: query } });
   }
 
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {PostService} from './post.service';
 import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
@@ -6,6 +6,7 @@ import {User} from '../login-basic/user';
 import {Post} from './post';
 import {Group} from '../group-structure/group';
 import {GroupService} from '../group-structure/group.service';
+import {PagedResourceCollection} from '@lagoshny/ngx-hateoas-client';
 
 
 @Component({
@@ -15,8 +16,14 @@ import {GroupService} from '../group-structure/group.service';
 })
 export class PostsComponent implements OnInit {
   public user: User = new User();
-  public group: Group = new Group();
-  public post: Post = new Post();
+  public posts: Post[] = [];
+  public totalPosts = 0;
+
+  /*Group sent via group-structure*/
+  @Input()
+  public group: Group;
+  @Input()
+  public father: Post;
 
   constructor(private route: ActivatedRoute,
               private groupService: GroupService,
@@ -27,9 +34,10 @@ export class PostsComponent implements OnInit {
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.postService.getResource('1').subscribe(
-      post => {
-        this.post = post;
+    this.postService.findByGroup2(this.group).subscribe(
+      (page: PagedResourceCollection<Post>) => {
+        this.posts = page.resources;
+        this.totalPosts = page.totalElements;
       });
   }
 
