a3688a5b611590c760b2894d99096710a6aa8fc3
marcresaenrich <marcresaenrich@gmail.com>
2022-05-11 21:51:04 +0200

    first version chat

--- src/app/chat/message-list/message-list.component.ts
+++ src/app/chat/message-list/message-list.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { MessageService } from '../message.service';
 import { Message } from '../message';
 import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import {User} from '../../login-basic/user';
 
 
 @Component({
@@ -11,30 +13,37 @@ import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  public user: User;
   public messages: Message[] = [];
-  public pageSize = 5;
-  public page = 1;
   public totalMessages = 0;
+  public pageSize = 5;
 
   constructor(
     public router: Router,
-    private messageService: MessageService) {
+    private messageService: MessageService,
+    private authenticationService: AuthenticationBasicService) {
   }
 
   ngOnInit(): void {
+    this.user = this.getCurrentUser();
     this.messageService.getPage({ pageParams:  { size: this.pageSize }, sort: { creationDate: 'ASC' } }).subscribe(
       (page: PagedResourceCollection<Message>) => {
         this.messages = page.resources;
         this.totalMessages = page.totalElements;
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < this.messages.length; i++){
+          this.messages[i].getRelation('author').subscribe((author: User) => {
+            this.messages[i].author = author;
+          });
+        }
       });
-  }
-
-  changePage(): void {
-    this.messageService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { creationDate: 'ASC' } }).subscribe(
-      (page: PagedResourceCollection<Message>) => this.messages = page.resources);
   }
 
   detail(message: Message): void {
     this.router.navigate(['messages', message.id]);
+  }
+
+  getCurrentUser(): User {
+    return new User(JSON.parse(localStorage.getItem('currentUser')));
   }
 }
