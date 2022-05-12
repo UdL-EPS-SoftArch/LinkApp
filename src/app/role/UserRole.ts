bcdc8dfdd96c7f10813a20c926118bbd6002aa25
Gerard <gerard@aules-107-061.udl.net>
2022-05-12 15:34:18 +0200

    created userrole classes

---
+++ src/app/role/UserRole.ts
@@ -0,0 +1,11 @@
+import {HateoasResource, Resource} from '@lagoshny/ngx-hateoas-client';
+import { User } from '../login-basic/user';
+import {Group} from '../group/Group';
+import {UserRoleEnum} from './UserRoleEnum';
+import {UserRoleKey} from './RoleKey';
+
+@HateoasResource('userroles')
+export class UserRole extends Resource  {
+  roleKey: UserRoleKey;
+  role: UserRoleEnum;
+}
