76291da84124d1f951fcbbae152c45f7afb0c98f
Gerard <gerard@aules-107-061.udl.net>
2022-05-12 15:38:38 +0200

    userrole service

---
+++ src/app/role/userrole.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { HateoasResourceOperation, ResourceCollection } from '@lagoshny/ngx-hateoas-client';
+import {UserRole} from './UserRole';
+
+@Injectable({providedIn: 'root'})
+export class UserService extends HateoasResourceOperation<UserRole> {
+
+  constructor() {
+    super(UserRole);
+  }
+
+}
