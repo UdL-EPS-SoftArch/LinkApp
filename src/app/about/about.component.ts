5be6b4dc7491b7f8b634b287e1401eead2e2dcba
bcaspi <balcp11@hotmail.com>
2022-05-19 20:12:24 +0200

    Initial search buttons working

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,30 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Group} from '../group/group';
+import {GroupService} from '../group/group.service';
+import {Router} from "@angular/router";
+import {PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  groups: Group[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalGroups = 0;
 
-  constructor() { }
+  constructor(public router: Router, private groupService: GroupService) {
+  }
+
+  ngOnInit(): void {
+    this.groupService.getPage({ pageParams:  { size: this.pageSize }, sort: { title: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Group>) => {
+        this.groups = page.resources;
+        this.totalGroups = page.totalElements;
+      });
+  }
+
 
 }
