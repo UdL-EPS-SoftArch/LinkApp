0ad27bba990a9fb64459e2679a067fdf8cf8634b
Sara Quejido <quejidosara@gmail.com>
2022-05-20 12:20:13 +0200

    Meet buttons in about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,6 +3,8 @@ import {Group} from '../group/group';
 import {GroupService} from '../group/group.service';
 import {Router} from "@angular/router";
 import {PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
+import {Meet} from "../meet/meet";
+import {MeetService} from "../meet/meet.service";
 
 @Component({
   selector: 'app-about',
@@ -11,11 +13,14 @@ import {PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
 })
 export class AboutComponent implements OnInit {
   groups: Group[] = [];
+  meets: Meet[] = [];
   public pageSize = 5;
   public page = 1;
   public totalGroups = 0;
+  public totalMeets = 0;
 
-  constructor(public router: Router, private groupService: GroupService) {
+
+  constructor(public router: Router, private groupService: GroupService, private meetService: MeetService) {
   }
 
   ngOnInit(): void {
@@ -23,6 +28,12 @@ export class AboutComponent implements OnInit {
       (page: PagedResourceCollection<Group>) => {
         this.groups = page.resources;
         this.totalGroups = page.totalElements;
+      });
+
+    this.meetService.getPage({ pageParams:  { size: this.pageSize }, sort: { title: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Meet>) => {
+        this.meets = page.resources;
+        this.totalMeets = page.totalElements;
       });
   }
 
